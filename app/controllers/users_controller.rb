92202ea6c03522a48af51bc60902c9d41be0a249
frann-y <fgranger00.com>
2019-02-22 00:01:38 +0000

    completed a user register page and a user login page

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,29 @@
+class UsersController < ApplicationController
+    
+    def new
+        #render the new form
+        @user = User.new
+    end
+
+    def create
+        user = User.new(user_params)
+        if user.save
+          session[:user_id] = user.id
+          redirect_to '/' #redirect_to root_path
+        else
+          redirect_to '/register'
+        end
+    end 
+
+    private
+
+    def user_params
+        params.require(:user).permit(
+            :first_name,
+            :last_name,
+            :email,
+            :password,
+            :password_confirmation,
+        )
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,16 @@ Rails.application.routes.draw do
     resources :categories, except: [:destroy]
   end
 
+  get '/register', to:'users#new'
+  post '/users', to: 'users#create'
+
+  get '/login', to:'sessions#new'
+  post '/sessions', to: 'sessions#create'
+  get '/logout', to:'sessions#destroy' 
+end
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
@@ -72,4 +82,4 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
+
