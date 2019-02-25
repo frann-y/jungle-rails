5af0a949faefb80563dea11f4aafb2024adcc812
frann-y <fgranger00.com>
2019-02-25 22:41:33 +0000

    fixed test cases for the products

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -8,58 +8,51 @@ require 'rails_helper'
 RSpec.describe Product, type: :model do
   describe 'Validations' do
     # validation tests/examples here
+    #moving everything in here from validations 
+  before(:each) do
+    @product = Product.new
+    @product.name = "Shoe"
+    @product.price = 25
+    @product.quantity = 5
+    #how to create a category & an object
+    @product.category = Category.new({name: "Apparel"})
+  end
 
     #Test to validates everything exists
     it "is valid if it all exists" do
     #create a product and create a whole object
-      @product = Product.new
-      @product.name = "Shoe"
-      @product.price = 25
-      @product.quantity = 5
-      #how to create a category & an object
-      @product.category = Category.new({name: "Apparel"})
       expect(@product).to be_valid
     end
 
     # validates :name, presence: true
     it "is not valid without a name" do
       #create a product and create a whole object
-      @product = Product.new
-      @product.name = "Sock"
-      @product.price = 10
-      @product.quantity = 10
-      @product.category = Category.new({name: "Apparel"})
-      expect(@product).to be_valid
+      #this test wants to fail
+      @product.name = nil
+      expect(@product).to_not be_valid
+      #an array to pront the error messages
+      #put in anywords in "" , then copy and paste error message into the ""
+      expect(@product.errors.full_messages).to include("Name can't be blank")
     end
 
     # validates :price, presence: true
     it "is not valid without a price" do
-      @product = Product.new
-      @product.name = "Tights"
-      @product.price = 20
-      @product.quantity = 20
-      @product.category = Category.new({name: "Apparel"})
-      expect(@product).to be_valid
+    #price is weird. it will convert it to cents. 
+      @product.price_cents = nil
+      expect(@product).to_not be_valid
     end
     # validates :quantity, presence: true
     it "is not valid without a quantity" do
-      @product = Product.new
-      @product.name = "Pantyhose"
-      @product.price = 15
-      @product.quantity = 15
-      @product.category = Category.new({name: "Apparel"})
-      expect(@product).to be_valid    
+      @product.quantity = nil
+      expect(@product).to_not be_valid    
     end
 
     # validates :category, presence: true
     it "is not valid without a category" do
-      @product = Product.new
-      @product.name = "Long Johns"
-      @product.price = 25
-      @product.quantity = 25
-      @product.category = Category.new({name: "Apparel"})
-      expect(@product).to be_valid
+      @product.quantity = nil
+      expect(@product).to_not be_valid #to_not be_valid?
     end
+
 
     #for each, pass in a block
     #it --name--- do
