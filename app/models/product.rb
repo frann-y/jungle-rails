f09b95a9c8c1920a4be6675af2832289f92a43bc
frann-y <fgranger00.com>
2019-02-25 03:54:42 +0000

    added average rating to product pages

--- app/models/product.rb
+++ app/models/product.rb
@@ -11,4 +11,9 @@ class Product < ActiveRecord::Base
   validates :quantity, presence: true
   validates :category, presence: true
 
+
+  def average_rating
+    self.reviews.average(:rating)
+  end
+
 end
